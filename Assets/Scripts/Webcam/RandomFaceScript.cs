f426d7fd027303f65f50d4543bf62efa570d0812
Sage Tesoro <satewarco@gmail.com>
2026-01-26 11:25:54 -0700

    face stuff

--- Assets/Scripts/Webcam/RandomFaceScript.cs
+++ Assets/Scripts/Webcam/RandomFaceScript.cs
@@ -2,27 +2,65 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class RandomFaceScript : MonoBehaviour {
     public TMP_Text textUI;
-    public Button button;
+    public Button startButton;
+    public Button changeFace;
+    public UDPExpressionReceiver expresionReceiver;
+
+    bool correctFace = false;
+    int currIndex;
 
     public enum NPCFace {
         Happy,
-        Sad,
-        surprised
-    }
+        Surprised,
+        Sad
+    };
 
     void Start() {
-        button.onClick.AddListener(() => {
-            StartCoroutine(EmotionsList());
-        });
+        startButton.onClick.AddListener(StartFaceMission);
+        //changeFace.onClick.AddListener(ChangeEmotion);
     }
 
-    IEnumerator EmotionsList() {
-        foreach (NPCFace face in System.Enum.GetValues(typeof(NPCFace))) {
-            textUI.text = face.ToString();
-            yield return new WaitForSeconds(1f);
+    void Update() {
+        // check if user is doing same emotion
+        if (expresionReceiver.expression == currIndex) {
+            correctFace = true;
         }
+    }
+
+    void ChangeEmotion() {
+        correctFace = true;
+    }
+
+    void StartFaceMission() {
+        startButton.gameObject.SetActive(false);
+        StartCoroutine(FaceMissionCoroutine());
+    }
+
+
+    IEnumerator FaceMissionCoroutine() {
+        int emotionCount = System.Enum.GetValues(typeof(NPCFace)).Length;
+
+        for (int index = 0; index < emotionCount; index++) {
+            // Show the current emotion
+            textUI.text = ((NPCFace)index).ToString();
+            currIndex = index;
+            // Wait until the player presses the correct emotion
+            correctFace = false;
+            while (!correctFace) {
+                yield return null; // wait for next frame
+            }
+
+            // Correct face detected
+            textUI.text = "Good Job!";
+            yield return new WaitForSeconds(2f); // wait 2 seconds before next emotion
+        }
+
+        // Mission finished
+        textUI.text = "All done!";
+        startButton.gameObject.SetActive(true); // optional: show button again
     }
 }
